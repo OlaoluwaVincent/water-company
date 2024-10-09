411dfb0f38a5f8ceb8e02cef222cc3b170e63c83
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-10-09 01:46:37 +0100

    Navbar, Sidebar, Content

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Menu from 'carbon-icons-svelte/lib/Menu.svelte';
+	import { themes } from '$lib/themes';
+	let selectedTheme = themes[11];
+
+	// Change theme function
+	function changeTheme(event: Event) {
+		const target = event.target as HTMLSelectElement;
+		selectedTheme = target.value;
+	}
+
+	onMount(() => {
+		if (typeof document !== 'undefined') {
+			document.body.setAttribute('data-theme', selectedTheme);
+		}
+	});
+
+	// Make sure the body has the correct theme
+	$: if (typeof document !== 'undefined') {
+		document.body.setAttribute('data-theme', selectedTheme);
+	}
+</script>
+
+<nav class="navbar bg-base-100 drop-shadow-md">
+	<label for="my-drawer-2" class="btn btn-ghost btn-circle drawer-button" >
+		<Menu size={32} />
+	</label>
+
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/about" class="link">About</a></li>
+			<li><a href="/settings" class="link">Settings</a></li>
+			<li>
+				<select name="theme" id="theme" on:change={changeTheme} class="link capitalize text-sm">
+					{#each themes as theme}
+						<option value={theme} selected={theme === selectedTheme}>{theme}</option>
+					{/each}
+				</select>
+			</li>
+		</ul>
+	</div>
+</nav>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let data: { content: string; title: string };
+</script>
+
+<div class="prose">
+	<h1>Hi there, this is the about page</h1>
+	<button class="btn">Click Daisy</button>
+	<h4>{data.title}</h4>
+</div>
