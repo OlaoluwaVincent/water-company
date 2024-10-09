975471cda0cb77413acd748c5c3b6a234c19d49d
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-10-09 10:25:46 +0100

    Add: Added theme selection to sidebar component

---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { themes } from '$lib/themes';
+
+	import CloseLarge from 'carbon-icons-svelte/lib/CloseLarge.svelte';
+
+	let selectedTheme = themes[11];
+
+	// Change theme function
+	function changeTheme(event: Event) {
+		const target = event.target as HTMLSelectElement;
+		selectedTheme = target.value;
+	}
+
+	onMount(() => {
+		if (typeof document !== 'undefined') {
+			document.body.setAttribute('data-theme', selectedTheme);
+		}
+	});
+
+	$: if (typeof document !== 'undefined') {
+		document.body.setAttribute('data-theme', selectedTheme);
+	}
+</script>
+
+<div class="drawer-side">
+	<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"> </label>
+	<aside class="menu bg-base-200 text-base-content min-h-full w-72 p-4 mt-0 relative">
+		<label
+			for="my-drawer-2"
+			aria-label="close sidebar"
+			class="absolute top-3 right-5 z-50 btn btn-ghost btn-circle drawer-overlay"
+		>
+			<CloseLarge size={20} />
+		</label>
+
+		<ul class="menu mt-8 rounded-box">
+			<li class=""><a href="/">Home</a></li>
+			<li class=""><a href="/about">About</a></li>
+            
+			<li>
+				<select name="theme" id="theme" on:change={changeTheme} class="capitalize text-sm">
+					{#each themes as theme}
+						<option value={theme} selected={theme === selectedTheme}>{theme}</option>
+					{/each}
+				</select>
+			</li>
+		</ul>
+	</aside>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,16 @@
-<script>
+<script lang="ts">
 	import '../app.css';
-	import CloseLarge from 'carbon-icons-svelte/lib/CloseLarge.svelte';
 	import Navbar from '$lib/components/navbar.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
 </script>
 
 <section class="drawer">
 	<input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-	<section class="drawer-content space-y-2">
+
+	<section class="drawer-content">
 		<Navbar />
 		<slot></slot>
 	</section>
 
-	<div class="drawer-side">
-		<label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"> </label>
-		<aside class="menu bg-base-200 text-base-content min-h-full w-72 p-4 mt-0 relative">
-			<label
-				for="my-drawer-2"
-				aria-label="close sidebar"
-				class="absolute top-3 right-5 z-50 btn btn-ghost btn-circle drawer-overlay"
-			>
-				<CloseLarge size={20} />
-			</label>
-
-			<ul class="menu mt-8 rounded-box">
-				<li class="!no-underline"><a href="/">Home</a></li>
-				<li class="!no-underline"><a href="/about">About</a></li>
-			</ul>
-		</aside>
-	</div>
+	<Sidebar />
 </section>
