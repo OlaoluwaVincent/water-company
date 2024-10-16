503b5dcf26cc6f1f9611d1aaee61959b41fda3b8
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-10-16 11:38:12 +0100

    done

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ import type { TypedPocketBase } from './types/pocketbase-types';
 import { env } from '$env/dynamic/public';
 
 const url =
-	env.PUBLIC_ENVIRONMENT == 'DEVELOPMENT' ? env.PUBLIC_LOCALPOCKET_URL : env.PUBLIC_POCKETHOST_UTL;
+	env.PUBLIC_ENVIRONMENT == 'DEVELOPMENT' ? env.PUBLIC_LOCALPOCKET_URL : env.PUBLIC_POCKETHOST_URL;
 
 const pb = new PocketBase(url) as TypedPocketBase;
 
