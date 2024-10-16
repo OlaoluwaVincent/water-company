da8ce852d0914c9ccb5658d22e7c4a38f79de85f
Olaoluwavincent <Olaoluwavincent@gmail.com>
2024-10-16 11:32:40 +0100

    fix: Deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,6 @@
 	import pb from '$lib/config';
 	import userStore from '$lib/stores/user';
 	import userTheme from '$lib/stores/theme';
-	import { closeModal, Modals } from 'svelte-modals';
 
 	// Define protected routes
 	const protectedRoutes = ['/orders', '/driver'];
@@ -64,12 +63,6 @@
 
 			<Sidebar />
 		</section>
-
-		<Modals>
-			<!-- svelte-ignore a11y-no-static-element-interactions -->
-			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<div slot="backdrop" class="backdrop" on:click={closeModal} />
-		</Modals>
 	{/if}
 
 	{#if !isReady}
@@ -78,14 +71,3 @@
 		</section>
 	{/if}
 </div>
-
-<style>
-	.backdrop {
-		position: fixed;
-		top: 0;
-		bottom: 0;
-		right: 0;
-		left: 0;
-		background: rgba(0, 0, 0, 0.5);
-	}
-</style>
